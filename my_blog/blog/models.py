c7df7072166f9a0a823cd43019b80811b4d80afd
gglsilva <gabrielchamex@gmail.com>
2021-12-29 19:34:19 -0300

    adding view, url and template post_detail, canonical urls

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -25,4 +26,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-        
+
+
+    # Return uma url canônica para o objeto.
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+        self.publish.month, self.publish.day, self.slug])
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/gabriel-ativ/Documentos/workspace_python/Django/exercicios/blogTudodedoce/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
